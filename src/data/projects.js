7a4b67811b6ab51b3406c0ae9f11968e4cd2a60c
juanjk24 <juanjkuellar24@gmail.com>
2025-08-06 18:46:49 -0500

    Add certificates and projects data for display in the application

---
+++ src/data/projects.js
@@ -0,0 +1,107 @@
+export const projects = [
+    {
+        title: "Software para la gestión de Operaciones de Transporte",
+        description: `Software que permite llevar el control de preoperacionales,
+        conductores, vehículos, programación de rutas entre otros. Realizado
+        por JnixSoft para clientes de la región del Putumayo en Colombia.`,
+        image: {
+            src: "/demo-jnixsoft.webp",
+            alt: "Software de Gestión de Transporte Screenshot",
+            width: 960,
+            height: 640,
+        },
+        links: [
+            {
+                href: "https://demo.jnixsoft.com/",
+                label: "Ver Demo",
+                icon: "external",
+            },
+        ],
+    },
+    {
+        title: "mementor",
+        description: `Tu mentor de prompts para cualquier IA.
+        Descubre prompts para potenciar tu creatividad y productividad.`,
+        image: {
+            src: "/shot-mementor.webp",
+            alt: "Screenshot de mementor, software descubrir prompts para potenciar tu creatividad y productividad.",
+            width: 960,
+            height: 720,
+        },
+        links: [
+            {
+                href: "https://mementor-eight.vercel.app",
+                label: "Ver Demo",
+                icon: "external",
+            },
+            {
+                href: "https://github.com/juanjk24/mementor",
+                label: "Código",
+                icon: "code",
+            },
+        ],
+    },
+    {
+        title: "Words Count",
+        description: `Contador de palabras, letras, espacios, mayúsculas,
+        minúsculas, números y caracteres.`,
+        image: {
+            src: "/shot-words-count.webp",
+            alt: "Screenshot de Words Count, software para contar palabras",
+            width: 960,
+            height: 720,
+        },
+        links: [
+            {
+                href: "https://words-count-omega.vercel.app/",
+                label: "Ver Demo",
+                icon: "external",
+            },
+            {
+                href: "https://github.com/juanjk24/words-count",
+                label: "Código",
+                icon: "code",
+            },
+        ],
+    },
+    {
+        title: "Remove Background",
+        description: `Aplicación para eliminar fondo de imágenes.`,
+        image: {
+            src: "/shot-removebg.webp",
+            alt: "Screenshot de software para eliminar fondo de imagenes",
+            width: 960,
+            height: 720,
+        },
+        links: [
+            {
+                href: "https://remover-fondo.vercel.app/",
+                label: "Ver Demo",
+                icon: "external",
+            },
+            {
+                href: "https://github.com/juanjk24/remove-bg",
+                label: "Código",
+                icon: "code",
+            },
+        ],
+    },
+    {
+        title: "Landing Page JnixSoft",
+        description: `Nueva Landing page de la empresa JnixSoft, empresa de desarrollo de
+        software en la región del Putumayo en Colombia.`,
+        image: {
+            src: "/landing-jnixsoft.webp",
+            alt: "Landing Page JnixSoft Screenshot",
+            width: 960,
+            height: 640,
+        },
+        links: [
+            {
+                href: "https://jnixsoft.vercel.app/",
+                label: "Ver Demo",
+                icon: "external",
+            },
+        ],
+    },
+];
