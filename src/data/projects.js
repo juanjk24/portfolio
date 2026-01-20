cea76524253649bc4039e62ea706c30683a23716
juanjk24 <juanjkuellar24@gmail.com>
2026-01-20 15:51:08 -0500

    Add new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,19 +19,71 @@ export const projects = [
         ],
     },
     {
+        title: "YachayCodex",
+        description: `Editor de código HTML, CSS y JavaScript en línea. Crea y edita tu código en tiempo real, únete a una sala para programar en equipo, resuelve retos y ejercicios de JavaScript, utiliza un chat de IA para generar código y comparte tus proyectos a través de un solo enlace.`,
+        image: {
+            src: "/shot-yachaycodex.webp",
+            alt: "Screenshot de Yachay Codex, editor de código en línea.",
+            width: 1920,
+            height: 1080,
+        },
+        links: [
+            {
+                href: "https://yachaycodex.shayddev.com",
+                label: "Ver Sitio",
+                icon: "external",
+            },
+        ],
+    },
+    {
+        title: "Impostor Game (+2000 visitas)",
+        description: "Impostor Game es un juego de deducción social para jugar con amigos. Descubre quién es el impostor usando pistas, lógica y engaño.",
+        image: {
+            src: "/shot-impostor-game.webp",
+            alt: "Screenshot de Impostor Game",
+            width: 1920,
+            height: 1280,
+        },
+        links: [
+            {
+                href: "https://impostores.online",
+                label: "Ver Sitio",
+                icon: "external",
+            },
+        ],
+    },
+    {
+        title: "Landing Page JnixSoft",
+        description: `Nueva Landing page de la empresa JnixSoft, empresa de desarrollo de
+        software en la región del Putumayo en Colombia.`,
+        image: {
+            src: "/landing-jnixsoft.webp",
+            alt: "Landing Page JnixSoft Screenshot",
+            width: 960,
+            height: 640,
+        },
+        links: [
+            {
+                href: "https://jnixsoft.vercel.app/",
+                label: "Ver Sitio",
+                icon: "external",
+            },
+        ],
+    },
+    {
         title: "mementor",
         description: `Tu mentor de prompts para cualquier IA.
         Descubre prompts para potenciar tu creatividad y productividad.`,
         image: {
             src: "/shot-mementor.webp",
             alt: "Screenshot de mementor, software descubrir prompts para potenciar tu creatividad y productividad.",
-            width: 960,
-            height: 720,
+            width: 1920,
+            height: 1080,
         },
         links: [
             {
                 href: "https://mementor-eight.vercel.app",
-                label: "Ver Demo",
+                label: "Ver Sitio",
                 icon: "external",
             },
             {
@@ -54,7 +106,7 @@ export const projects = [
         links: [
             {
                 href: "https://words-count-omega.vercel.app/",
-                label: "Ver Demo",
+                label: "Ver Sitio",
                 icon: "external",
             },
             {
@@ -76,31 +128,13 @@ export const projects = [
         links: [
             {
                 href: "https://remover-fondo.vercel.app/",
-                label: "Ver Demo",
+                label: "Ver Sitio",
                 icon: "external",
             },
             {
                 href: "https://github.com/juanjk24/remove-bg",
                 label: "Código",
                 icon: "code",
-            },
-        ],
-    },
-    {
-        title: "Landing Page JnixSoft",
-        description: `Nueva Landing page de la empresa JnixSoft, empresa de desarrollo de
-        software en la región del Putumayo en Colombia.`,
-        image: {
-            src: "/landing-jnixsoft.webp",
-            alt: "Landing Page JnixSoft Screenshot",
-            width: 960,
-            height: 640,
-        },
-        links: [
-            {
-                href: "https://jnixsoft.vercel.app/",
-                label: "Ver Demo",
-                icon: "external",
             },
         ],
     },
