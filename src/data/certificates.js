a71464f947f255c92c3bd55d7ca532f84464e432
juanjk24 <juanjkuellar24@gmail.com>
2026-02-17 23:23:14 -0500

    Añadir mencion de honor

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,42 +1,47 @@
 export const certificates = [
-    {
-      title: "Node.js Avanzado",
-      institution: "Platzi",
-      link: "/certificates/diploma-nodejs-avanzado.pdf",
-    },
-    {
-      title: "Programación Web Full Stack",
-      institution: "Universidad de Antioquia (UDEA)",
-      link: "/certificates/diploma-programacion-web-udea.pdf",
-    },
-    {
-      title: "Fundamentos de Node.js",
-      institution: "Platzi",
-      link: "/certificates/diploma-nodejs.pdf",
-    },
-    {
-      title: "Curso Profesional de Git y GitHub",
-      institution: "Platzi",
-      link: "/certificates/diploma-git-github.pdf",
-    },
-    {
-      title: "Web Components con JavaScript",
-      institution: "Platzi",
-      link: "/certificates/diploma-web-components.pdf",
-    },
-    {
-      title: "Fundamentos de JavaScript",
-      institution: "Platzi",
-      link: "/certificates/diploma-javascript.pdf",
-    },
-    {
-      title: "Fundamentos de Ingeniería de Software",
-      institution: "Platzi",
-      link: "/certificates/diploma-ingenieria.pdf",
-    },
-    {
-      title: "Programación Básica",
-      institution: "Platzi",
-      link: "/certificates/diploma-programacion-basica.pdf",
-    },
-  ];
+  {
+    title: "Node.js Avanzado",
+    institution: "Platzi",
+    link: "/certificates/diploma-nodejs-avanzado.pdf",
+  },
+  {
+    title: "Mención de Honor, Tecnología en Desarrollo de Software",
+    institution: "Institución Universitaria del Putumayo (UniPutumayo)",
+    link: "/certificates/mencion-honor.pdf"
+  },
+  {
+    title: "Programación Web Full Stack",
+    institution: "Universidad de Antioquia (UDEA)",
+    link: "/certificates/diploma-programacion-web-udea.pdf",
+  },
+  {
+    title: "Fundamentos de Node.js",
+    institution: "Platzi",
+    link: "/certificates/diploma-nodejs.pdf",
+  },
+  {
+    title: "Curso Profesional de Git y GitHub",
+    institution: "Platzi",
+    link: "/certificates/diploma-git-github.pdf",
+  },
+  {
+    title: "Web Components con JavaScript",
+    institution: "Platzi",
+    link: "/certificates/diploma-web-components.pdf",
+  },
+  {
+    title: "Fundamentos de JavaScript",
+    institution: "Platzi",
+    link: "/certificates/diploma-javascript.pdf",
+  },
+  {
+    title: "Fundamentos de Ingeniería de Software",
+    institution: "Platzi",
+    link: "/certificates/diploma-ingenieria.pdf",
+  },
+  {
+    title: "Programación Básica",
+    institution: "Platzi",
+    link: "/certificates/diploma-programacion-basica.pdf",
+  },
+];
